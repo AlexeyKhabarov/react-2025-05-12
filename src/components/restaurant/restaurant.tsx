e7524ac54f5ecd776279c3754cdb790b6a243626
AlexeyHabarov <alexey.habarov@diwo.tech>
2025-05-28 21:56:43 +0300

    hw5 added progressBar, added module.css

--- src/components/restaurant/restaurant.tsx
+++ src/components/restaurant/restaurant.tsx
@@ -2,7 +2,7 @@ import type { RestaurantItem } from "../../types/restaurants";
 import { Menu } from "../menu/menu";
 import { Reviews } from "../reviews/reviews";
 import { ReviewForm } from "../reviewForm/reviewForm";
-import "./restaurant.css";
+import style from "./restaurant.module.css";
 
 type RestaurantProps = {
   restaurant: RestaurantItem;
@@ -11,12 +11,12 @@ type RestaurantProps = {
 export const Restaurant = ({ restaurant }: RestaurantProps) => {
   const { name, menu, reviews } = restaurant;
   return (
-    <div className="restaurant">
-      <h2 className="restaurant__title">{name}</h2>
+    <div className={style.restaurant}>
+      <h2 className={style.restaurantTitle}>{name}</h2>
       <Menu menu={menu} />
       <Reviews reviews={reviews} />
-      <div className="restaurant__section">
-        <h3 className="restaurant__subtitle">Leave feedback</h3>
+      <div className={style.restaurantSection}>
+        <h3 className={style.restaurantSubtitle}>Leave feedback</h3>
         <ReviewForm />
       </div>
     </div>
